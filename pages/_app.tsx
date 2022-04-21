82377bf517a343ad8dc6d64979b92e9412e6acd1
“Jose <ivjosesantos@gmail.com>
2022-04-21 19:44:47 +0800

    husky and lint-stage setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const data = ' '
   return <Component {...pageProps} />
 }
 
